b59faf3d8dfc937070d1069ea1ae3e9a15991cc9
BayuSantoso23 <alex74579@gmail.com>
2024-12-04 20:22:17 +0700

    Final (Adding Comment)

--- js/index.js
+++ js/index.js
@@ -1,30 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Mengambil berbagai input dari html
     const beratBadanInput = document.getElementById("beratBadan");
     const tinggiBadanInput = document.getElementById("tinggiBadan");
     const usiaInput = document.getElementById("usia");
     const hitungButton = document.querySelector(".green-button");
     const resetButton = document.querySelector(".red-button");
+    const jenisKelamin = document.getElementById("jenisKelamin");
   
+  // Deklarasi variabel hasil dan informasi yang akan ditampilkan di page utama
     const resultCalculation = document.getElementById("result-calculation");
     const infoResult = document.getElementById("info-result");
   
+  // Menghitung BMI
     hitungButton.addEventListener("click", function (event) {
       event.preventDefault(); 
   
+      // Mengubah input ke dalam bentuk float
       const beratBadan = parseFloat(beratBadanInput.value);
       const tinggiBadan = parseFloat(tinggiBadanInput.value) / 100; 
   
+      // Validasi apabila input tidak valid (tidak berisikan angka, termasuk jika isi adalah kosong)
       if (isNaN(beratBadan) || isNaN(tinggiBadan) || tinggiBadan <= 0) {
         alert("Harap masukkan data berat dan tinggi badan yang valid.");
         return;
       }
   
+      // Deklarasi rumus dan hasil
       const bmi = (beratBadan / (tinggiBadan * tinggiBadan)).toFixed(1);
       resultCalculation.textContent = bmi;
   
+      // Status dan info yang ingin di tampilkan di halaman utama
       let status = "";
       let info = "";
   
+      // Cek kondigi bmi untuk ditampilkan ke halaman utama
       if (bmi < 18.5) {
         status = "Kekurangan berat badan";
         info = "Disarankan untuk meningkatkan asupan kalori dan berkonsultasi dengan ahli gizi.";
@@ -46,17 +55,24 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementById("anjuran-bmi").textContent = `Jika BMI Anda berada di dalam kategori ini maka Anda dianjurkan ${info}`;
     });
   
+    // Reset button yang menghilangkan input jika diclik
     resetButton.addEventListener("click", function (event) {
       event.preventDefault(); 
-
+    
+      // Reset semua input
       beratBadanInput.value = "";
       tinggiBadanInput.value = "";
       usiaInput.value = "";
+      jenisKelamin.value = ""; 
+    
+      // Reset hasil dan informasi
       resultCalculation.textContent = "0";
       infoResult.textContent = "Anda memiliki berat badan #";
-      document.getElementById("hasil-bmi").textContent = "Hasil BMI diantara #";
-      document.getElementById("kategori-bmi").textContent = "Anda berada dalam kategori #";
-      document.getElementById("anjuran-bmi").textContent = "Jika BMI Anda berada di dalam kategori ini maka Anda dianjurkan #";
+    
+      document.getElementById("hasil-bmi").textContent = "";
+      document.getElementById("kategori-bmi").textContent = "";
+      document.getElementById("anjuran-bmi").textContent = "";
     });
+    
   });
   
